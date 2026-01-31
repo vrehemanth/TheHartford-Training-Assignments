9901a049743ecc225d6615c7ef6d23fac7f1c30b
vrehemanth <vrehemanth@gmail.com>
2026-02-01 00:07:57 +0530

    Weekly-Assignment-5-Exercise-5-Updated

--- .NET-Weekly-Assignments/Week-5-Assignment/Exercises/Exercise-5/Program.cs
+++ .NET-Weekly-Assignments/Week-5-Assignment/Exercises/Exercise-5/Program.cs
@@ -1,4 +1,22 @@
-﻿namespace Exercise_5
+﻿/*
+    Boxing is a marƟal art and combat sport in which two people throw punches at each other, usually
+    with gloved hands. Historically, the goals have been to weaken and knock down the opponent. In
+    December 2025, the World Boxing Council, World Boxing AssociaƟon and the InternaƟonal Boxing
+    FederaƟon reached an agreement to standardize the names for January 2026 onwards, of the different
+    weight categories under which Boxing matches are conducted.
+
+    John is a good boxer and he wants to know under what category he should enrol his name.
+    Can you please help him out?
+
+    Input Format:
+    Input consists of an integer that corresponds to John's weight.
+
+    Output Format:
+    Output consists of a string that corresponds to any of the category names listed above or “Invalid Input”.
+    Print Invalid Input if the weight is negative or if the weight is greater than 120. 
+*/
+
+namespace Exercise_5
 {
     internal class Program
     {
