ded539261bb39b83ce2d79b91f9c52fb35443f64
vrehemanth <vrehemanth@gmail.com>
2026-02-13 09:42:34 +0530

    Updated: .NET-Day-11-Assignment

--- .NET-Assignments/Day-11/Products/Products/DTOs/ProductCreateDTO.cs
+++ .NET-Assignments/Day-11/Products/Products/DTOs/ProductCreateDTO.cs
@@ -1,10 +1,21 @@
-﻿namespace Products.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.DTOs
 {
     public class ProductCreateDTO
     {
+        [Required(ErrorMessage = "Product name is required.")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Stock quantity is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity cannot be negative.")]
         public int StockQuantity { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
         public string? Category { get; set; }
     }
 }
