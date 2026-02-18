48f7b7f72f1c24e830fdca2ad57dd80f4b03b775
vrehemanth <vrehemanth@gmail.com>
2026-02-18 10:13:28 +0530

    Updated: Dotnet-Day-14-Assignments

--- .NET-Assignments/Day-14/JWTAuth/JWTAuth/Models/JwtSettings.cs
+++ .NET-Assignments/Day-14/JWTAuth/JWTAuth/Models/JwtSettings.cs
@@ -2,11 +2,11 @@
 {
     public class JwtSettings
     {
-        public string? SecretKey { get; set; }
+        public string SecretKey { get; set; }
 
-        public string? Issuer { get; set; }
+        public string Issuer { get; set; }
 
-        public string? Audience { get; set; }
+        public string Audience { get; set; }
 
         public int ExpiryMinutes { get; set; }
     }
