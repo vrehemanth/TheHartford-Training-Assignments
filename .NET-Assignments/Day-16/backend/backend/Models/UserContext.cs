d26207ab82bb11134ae8dbd08ca71d4430691c9c
vrehemanth <vrehemanth@gmail.com>
2026-02-19 22:58:09 +0530

    Added: Dotnet-Day-16-Assignments

---
+++ .NET-Assignments/Day-16/backend/backend/Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ .NET-Assignments/Day-16/backend/backend/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
